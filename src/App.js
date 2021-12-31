c6e5caf4e429029e389215e59f7047ad4eab2ee4
sahaadrita <adrita.saha@optimizeitsystems.com>
2021-12-31 15:37:10 +0530

    Today

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React, { Fragment } from 'react';
+import Meals from './components/Meals/Meals';
+
+import Header from './components/Layout/Header';
+
+function App() {
+  return (
+    <Fragment>
+       <Header/>
+       <main>
+         <Meals/>
+       </main>
+    </Fragment>
+    
+ 
+  );
+}
+
+export default App;
