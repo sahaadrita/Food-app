b60479c2eaaaad9547b019dc6d6601fa862464ca
sahaadrita <adrita.saha@optimizeitsystems.com>
2022-01-04 17:09:03 +0530

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Meals from './components/Meals/Meals';
-
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 
 function App() {
+const[cartIsShown, setCartIsShown]=useState(false)
+const showCartHandler=()=>{
+  setCartIsShown(true);
+}
+const hideCartHandler=()=>{
+  setCartIsShown(false);
+}
   return (
     <Fragment>
-       <Header/>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      
+       <Header onShowCart={showCartHandler}/>
        <main>
          <Meals/>
        </main>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,20 @@
+import classes from './Cart.module.css'
+import Modal from '../UI/Modal';
+const Cart=props=>{
+    const CartItems=<ul className={classes['cart-items']}>{
+    [{id:'c1', mame:'Sushi', amount:'2', price:'12.99'}].map(item=><li>{item.name}</li>)}</ul>;
+ return(
+     <Modal onClose={props.onClose}>
+         {CartItems}
+         <div className={classes.total}>
+             <span>Total Amount</span>
+             <span>35.62</span>
+         </div>
+         <div className={classes.actions}>
+             <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+             <button className={classes.button}>Order</button>
+         </div>
+     </Modal>
+ )
+}
+export default Cart;
